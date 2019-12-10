62a64ed1135013a1650e5d3f2d0782fff237d474
Marcin Swiderski <marcin.swiderski@gmail.com>
2019-12-10 13:51:06 +0100

    Day10

---
+++ 10/day10.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"math/cmplx"
+	"os"
+	"sort"
+)
+
+type AsteroidMap [][]int
+
+type Point struct {
+	x, y int
+}
+
+func ReadMap(reader io.Reader) AsteroidMap {
+	var line string
+	field := AsteroidMap{}
+	for y := 0; ; y++ {
+		n, err := fmt.Fscanln(reader, &line)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			fmt.Errorf("Error during read %s", err)
+			break
+		}
+		field = append(field, make([]int, len(line)))
+		for x, c := range line {
+			point := 0
+			if c == '#' {
+				point = 1
+			}
+			field[y][x] = point
+		}
+	}
+	return field
+}
+
+func (field AsteroidMap) FindBestAsteroid() (int, int, int) {
+	max_pos := struct{ x, y int }{}
+	max_number := 0
+	for y := 0; y < len(field); y++ {
+		for x := 0; x < len(field[y]); x++ {
+			if field[y][x] == 1 {
+				points := field.findVisibleAsteroids(x, y)
+				if len(points) > max_number {
+					max_number = len(points)
+					max_pos.x = x
+					max_pos.y = y
+				}
+			}
+		}
+	}
+	return max_pos.x, max_pos.y, max_number
+}
+
+func (field AsteroidMap) findVisibleAsteroids(x, y int) []complex128 {
+	points := []complex128{}
+	for row := 0; row < len(field); row++ {
+		for col := 0; col < len(field[row]); col++ {
+			if row == y && col == x {
+				continue
+			}
+			if field[row][col] == 0 {
+				continue
+			}
+			if field.isVisible(x, y, col, row) {
+				//fmt.Println(row, col, "isVisible from", x, y)
+				points = append(points, complex(float64(col), float64(row)))
+			}
+		}
+	}
+	return points
+}
+
+func (field AsteroidMap) isVisible(x1, y1, x2, y2 int) bool {
+	//fmt.Println("isVisible", x1, y1, x2, y2)
+	dx, dy := x2-x1, y2-y1
+	gcd := GCD(dx, dy)
+	//fmt.Println("dx", dx, "dy", dy)
+	if gcd < 0 {
+		gcd = -gcd
+	}
+	if gcd > 0 {
+		//fmt.Println("GCD", gcd)
+		dx /= gcd
+		dy /= gcd
+	}
+	if dy == 0 {
+		if dx > 0 {
+			dx = 1
+		} else {
+			dx = -1
+		}
+	}
+	if dx == 0 {
+		if dy > 0 {
+			dy = 1
+		} else {
+			dy = -1
+		}
+	}
+	//fmt.Println("dx", dx, "dy", dy)
+	for {
+		x1 += dx
+		y1 += dy
+		//fmt.Println(x1, y1)
+		if x1 == x2 && y1 == y2 {
+			break
+		}
+		if field[y1][x1] == 1 {
+			//fmt.Println("Obstacle at ", x1, y1)
+			return false
+		}
+	}
+
+	return true
+}
+
+func GCD(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func main() {
+	field := ReadMap(os.Stdin)
+	x, y, n := field.FindBestAsteroid()
+	fmt.Println(x, y, n)
+	points := field.findVisibleAsteroids(x, y)
+	asteroid := complex(float64(x), float64(y))
+	sort.Slice(points, func(i, j int) bool {
+		// normalize the point against the asteroid, rotate clockwise and get the phase
+		return cmplx.Phase((points[i]-asteroid)*-1i) < cmplx.Phase((points[j]-asteroid)*-1i)
+	})
+	fmt.Println(points[199])
+}
---
+++ 10/day10_test.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+	"strings"
+	"testing"
+)
+
+func TestReadMap(t *testing.T) {
+	// t.Fatal("not implemented")
+	t.Run("large field", func(t *testing.T) {
+		reader := strings.NewReader(`.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##`)
+		field := ReadMap(reader)
+		if len(field) != 20 {
+			t.Errorf("Invalid field height: %d", len(field))
+		}
+		if len(field[0]) != 20 {
+			t.Errorf("Invalid field width: %d", len(field))
+		}
+	})
+}
+
+func TestFindBestAsteroid(t *testing.T) {
+	var test_data = []struct {
+		data    string
+		x, y, n int
+	}{
+		{`.#..#
+.....
+#####
+....#
+...##`, 3, 4, 8},
+		{`......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####`, 5, 8, 33},
+		{`#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.`, 1, 2, 35},
+		{`.#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..`, 6, 3, 41},
+		{`.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##`, 11, 13, 210},
+	}
+
+	for idx := range test_data {
+		i := idx
+		t.Run(fmt.Sprintf("example %d", i), func(t *testing.T) {
+			field := ReadMap(strings.NewReader(test_data[i].data))
+			x, y, number := field.FindBestAsteroid()
+			if x != test_data[i].x || y != test_data[i].y || number != test_data[i].n {
+				t.Errorf("Invalid output for %v, %d, %d, %d", test_data[i], x, y, number)
+			}
+		})
+	}
+}
+
+func TestIsVisible(t *testing.T) {
+	field := ReadMap(
+		strings.NewReader(
+			`.#..#
+.....
+#####
+....#
+...##`))
+	t.Run("0,1 -> 1,1", func(t *testing.T) {
+		if !field.isVisible(0, 2, 1, 2) {
+			t.Error("Not visible")
+		}
+	})
+	t.Run("0,2 -> 2,2", func(t *testing.T) {
+		if field.isVisible(0, 2, 2, 2) {
+			t.Error("visible")
+		}
+	})
+	t.Run("1,2 -> 0,2", func(t *testing.T) {
+		if !field.isVisible(1, 2, 0, 2) {
+			t.Error("not visible")
+		}
+	})
+	t.Run("0,2 -> 4,2", func(t *testing.T) {
+		if field.isVisible(0, 2, 4, 2) {
+			t.Error("visible")
+		}
+	})
+	t.Run("0,2 -> 3,4", func(t *testing.T) {
+		if !field.isVisible(0, 2, 3, 4) {
+			t.Error("not visible")
+		}
+	})
+	t.Run("Find visible asteroids", func(t *testing.T) {
+		n := field.findVisibleAsteroids(3, 4)
+		if len(n) != 8 {
+			t.Errorf("Invalid number of visible asteroids %d", len(n))
+		}
+	})
+	t.Run("Find visible asteroids 1, 2", func(t *testing.T) {
+		n := field.findVisibleAsteroids(1, 2)
+		if len(n) != 7 {
+			t.Errorf("Invalid number of visible asteroids %d", len(n))
+		}
+	})
+}
+
+func TestPoint(t *testing.T) {
+	fmt.Println(cmplx.Abs(1 + 1i))
+	fmt.Println(cmplx.Phase((0 + 1i) * 1i))
+	fmt.Println(cmplx.Phase((1) * 1i))
+}
